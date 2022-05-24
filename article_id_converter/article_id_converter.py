7feb9dd87a93463c4e517f49800cee7eae1aff9b
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-05-24 17:28:19 +0200

    Use Bioregistry to normalize identifier types

--- article_id_converter/article_id_converter.py
+++ article_id_converter/article_id_converter.py
@@ -1,6 +1,9 @@
 """Main module."""
 import requests
 from collections import defaultdict
+import bioregistry
+
+id2property = {"pmc": "P932", "doi": "P356", "pubmed": "P698"}
 
 
 def convert_ids(list_of_ids, input_id="DOI", output_id="PMCID", return_type="list"):
@@ -8,7 +11,7 @@ def convert_ids(list_of_ids, input_id="DOI", output_id="PMCID", return_type="lis
     Obtains a list of output IDs from Wikidata given a list of input IDs.
     Args:
         list_of_ids (list): A list of IDs as strings.
-        input_id (str): The kind of IDs in the input. One of ["PMID", "PMCID", "QID" or "DOI"]
+        input_id (str): The kind of IDs in the input. One of ["PMID", "PMCID", "WIKIDATA" or "DOI"]
         output_id (str): The kind of IDs to be returned in the output. One of ["PMID", "PMCID", "QID" or "DOI"]
         return_type (str): One of "list" or "data.frame". If "DataFrame", the output includes a column with the input data.
 
@@ -17,30 +20,38 @@ def convert_ids(list_of_ids, input_id="DOI", output_id="PMCID", return_type="lis
         OR
         (DataFrame): A pandas Data
     """
+    input_id_norm = bioregistry.norm_prefix(input_id)
+    if input_id_norm is None:
+        raise ValueError
+    input_property = id2property[input_id_norm]
 
-    id2property = {"PMCID": "P932", "DOI": "P356", "PMID": "P698"}
+    output_id_norm = bioregistry.norm_prefix(output_id)
+    if output_id_norm is None:
+        raise ValueError
+    output_property = id2property[output_id_norm]
+    
     values = ""
 
     list_of_ids = [str(i).upper() for i in list_of_ids]
-    if input_id == "QID":
+    if input_id_norm == "wikidata":
         for id in list_of_ids:
             values = values + f' "wd:{id}"'
         middle_logic = f"""
-         ?input_id_temp wdt:{id2property[output_id]} ?output_id_temp .
+         ?input_id_temp wdt:{output_property} ?output_id_temp .
          """
     else:
         for id in list_of_ids:
             values = values + f' "{id}"'
 
-        if output_id == "QID":
+        if output_id_norm == "wikidata":
             middle_logic = f"""
-         ?output_id_temp wdt:{id2property[input_id]} ?input_id_temp .
+         ?output_id_temp wdt:{input_property} ?input_id_temp .
          """
         else:
 
             middle_logic = f"""
-         ?item wdt:{id2property[input_id]} ?input_id_temp .
-         ?item wdt:{id2property[output_id]} ?output_id_temp .
+         ?item wdt:{input_property} ?input_id_temp .
+         ?item wdt:{output_property} ?output_id_temp .
          """
 
     query = f"""
